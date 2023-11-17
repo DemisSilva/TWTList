57530f5694a84b0d2a25edb77fb3c697c8ecedf5
DemisSilva <demisouzasilva@gmail.com>
2023-11-17 17:41:23 -0300

    Adicionando a funcionalidade de conclusão de tarefa

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -81,4 +81,18 @@ public class TodoController : Controller
         return RedirectToAction(nameof(Index));
      }
 
+     public IActionResult ToComplete(int id)
+     {
+        var todo  = _context.Todos.Find(id);
+        if(todo is null)
+        {
+            return NotFound();
+        }
+
+        todo.IsCompleted = true;
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+     }
+
 }
