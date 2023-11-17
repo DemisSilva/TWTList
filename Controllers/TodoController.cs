c090eeb58a2e5857eaac6fd302005d706c137f8a
DemisSilva <demisouzasilva@gmail.com>
2023-11-17 17:29:17 -0300

    Adicionando funcionalidade de Edit

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -52,4 +52,33 @@ public class TodoController : Controller
         return RedirectToAction(nameof(Index));
     }
 
+    public IActionResult Edit(int id)
+    {
+
+        var todo  = _context.Todos.Find(id);
+
+        if(todo is null)
+        {
+            return NotFound();
+        }
+
+        var viewModel = new EditTodoViewModel {Title = todo.Title, Date = todo.Date};
+        ViewData["title"] = "Editar Tarefa";
+        return View(viewModel);
+    }
+    [HttpPost]
+     public IActionResult Edit(int id, EditTodoViewModel model)
+     {
+        var todo  = _context.Todos.Find(id);
+        if(todo is null)
+        {
+            return NotFound();
+        }
+        todo.Title = model.Title;
+        todo.Date = model.Date;
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+     }
+
 }
---
+++ ViewModels/EditTodoViewModel.cs
@@ -0,0 +1,9 @@
+namespace TWTodoList.ViewModels;
+using Models;
+
+
+public class EditTodoViewModel
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
